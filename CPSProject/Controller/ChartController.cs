56c62d991ace69c0e41c007c3153657cea4572fc
Paweł Młynarczyk <210278@edu.p.lodz.pl>
2019-03-21 02:37:53 +0100

    Adding first logic classes

---
+++ CPSProject/Controller/ChartController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CPSProject.Controller
+{
+    public class ChartController
+    {
+        private ICommand drawCommand;
+
+        public ICommand DrawCommand
+        {
+            get
+            {
+                if(drawCommand == null)
+                {
+                    drawCommand = new RelayCommand(
+                        param => Draw(),
+                        param => CanDraw());
+                }
+                return drawCommand;
+            }
+        }
+
+        private int amplitude;
+        private int startingMoment;
+        private int duration;
+
+        public int FirstChartID { get; set; } = -1;
+        public string AmplitudeText { get; set; }
+        public string StartingMomentText { get; set; }
+        public string DurationText { get; set; }
+
+
+        private bool CanDraw()
+        {
+            if (FirstChartID == -1) return false;
+            if (FirstChartID == 0)
+            {
+                if (!int.TryParse(AmplitudeText, out amplitude)) return false;
+                if (!int.TryParse(StartingMomentText, out startingMoment)) return false;
+                if (!int.TryParse(DurationText, out duration)) return false;
+            }
+            return true;
+        }
+
+        private void Draw()
+        {
+            Console.WriteLine(amplitude + " " + startingMoment + " " + duration);
+        }
+    }
+}
